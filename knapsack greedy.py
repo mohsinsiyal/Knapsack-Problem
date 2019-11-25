7830971485ee66137aea2c8eb95b2ad3aa58070d
Mohsin Siyal <dev.mohsinsiyal@gmail.com>
2019-11-25 18:15:59 +0500

    Just changed number of iterations

--- knapsack greedy.py
+++ knapsack greedy.py
@@ -57,5 +57,5 @@ names = ['wine', 'beer', 'pizza', 'burger', 'fries',
 values = [89,90,95,100,90,79,50,10]
 calories = [123,154,258,354,365,150,95,195]
 foods = buildMenu(names, values, calories)
-testGreedys(foods, 750)
+testGreedys(foods, 850)
     
